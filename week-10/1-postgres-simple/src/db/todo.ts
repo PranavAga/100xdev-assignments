01e1a07d789ae9687e5b5ff1f671871073b4667a
Pranav Agarwal <94969289+PranavAga@users.noreply.github.com>
2024-06-01 21:30:08 +0530

    completed 1

--- week-10/1-postgres-simple/src/db/todo.ts
+++ week-10/1-postgres-simple/src/db/todo.ts
@@ -10,7 +10,22 @@ import { client } from "..";
  * }
  */
 export async function createTodo(userId: number, title: string, description: string) {
-    
+    try {
+        const values = [userId, title, description];
+        const queryText = `
+        INSERT
+        INTO todos(user_id, title, description) 
+        VALUES($1, $2, $3)
+        RETURNING *;
+        `
+        const res = await client.query(queryText,values);
+        // console.log(res.rows);
+        return res.rows[0];
+
+    } catch (error) {
+        console.log(error);
+        // return new Error(error)
+    }
 }
 /*
  * mark done as true for this specific todo.
@@ -23,7 +38,21 @@ export async function createTodo(userId: number, title: string, description: str
  * }
  */
 export async function updateTodo(todoId: number) {
-
+    try {
+        const values = [todoId];
+        const queryText = `
+        UPDATE todos
+        SET done = true
+        WHERE id = $1
+        RETURNING *;
+        `
+        const res = await client.query(queryText,values);
+        return res.rows[0];
+        
+    } catch (error) {
+        console.log(error);
+        return new Error('Error in updateTodos')
+    }
 }
 
 /*
@@ -37,5 +66,18 @@ export async function updateTodo(todoId: number) {
  * }]
  */
 export async function getTodos(userId: number) {
+    // try {
+        const values = [userId];
+        const queryText = `
+        SELECT * 
+        FROM todos 
+        WHERE user_id = $1
+        `
+        const res = await client.query(queryText,values);
+        return res.rows;
 
+    // } catch (error) {
+    //     console.log(error);
+    //     return new Error('Error in getTodos')
+    // }
 }
--- week-10/1-postgres-simple/src/db/user.ts
+++ week-10/1-postgres-simple/src/db/user.ts
@@ -10,7 +10,18 @@ import { client } from "..";
  * }
  */
 export async function createUser(username: string, password: string, name: string) {
-    
+    try {
+        const values = [username, password, name];
+        const queryText = `
+        INSERT INTO users(username, password, name) 
+        VALUES($1, $2, $3);
+        `
+        const res = await client.query(queryText,values);
+        return res.rows[0];
+    } catch (error) {
+        console.log(error);
+        return new Error('Error in createUser')
+    }
 }
 
 /*
@@ -22,5 +33,18 @@ export async function createUser(username: string, password: string, name: strin
  * }
  */
 export async function getUser(userId: number) {
-    
+    try {
+        const values = [userId];
+        const queryText = `
+        SELECT * 
+        FROM users 
+        WHERE id = $1
+        `
+        const res = await client.query(queryText,values);
+        return res.rows[0];
+
+    } catch (error) {
+        console.log(error);
+        return new Error('Error in getUser')
+    }
 }
